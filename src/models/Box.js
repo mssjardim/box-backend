ffe9e6c48f2b56e29f8c6c2f8962af0641c47da9
Marco Santarelle da Silva Jardim <marco.j@samsung.com>
2019-04-12 17:19:45 -0400

    Initial commit Box Backend

---
+++ src/models/Box.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Box = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        files: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'File' }]
+    }, {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model('Box', Box);
